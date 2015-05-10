31804156300d9d46439ace23f1108a3d8550f0a9
tencent1234 <1771861981@qq.com>
2015-05-10 18:02:08 +0800

    no message

---
+++ 04/main.py
@@ -0,0 +1,72 @@
+class Dimension: 
+  def_init_(self,x,y):
+  
+    self.x=x
+    
+    self.y=y
+
+    
+def area(self):
+        
+pass
+
+class circle(Dimension):
+    
+     def_init_(self,r):
+        
+Dimension._int_(self,r,0)
+    def area(self):
+        
+return 3.14*self.x*self.x
+class ellipse(Dimension):
+    
+
+def_init_(self,w,h):
+
+         
+Dimension._int_(self,w,h)
+    def area(self):
+        
+return 3.14*self.x*self.y
+class square(Dimension):
+    
+def_init_(self,a,0):
+        
+Dimension._int_(self,a,0)
+    def area(self):
+        
+return self.x*self.x    
+
+class rectangle(Dimension):
+    
+def_init_(self,c,d):
+        
+Dimension._int_(self,c,d)
+    def area(self):
+        
+return self.x*self.y
+d1=circle(5.0)
+
+d2=ellipse(10.0,20.0)
+
+d3=square(15)
+
+d4=rectangle(20.0,15.0)
+
+def compute_area(shapes)
+
+    
+
+
+
+
+shapes=d1.area()+d2.area()+d3.area()+d4.area()
+
+    print(shapes) 
+
+    
+
+
+
+
+    
